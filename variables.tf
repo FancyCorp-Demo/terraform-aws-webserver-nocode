f50eb5a3ebe99801cc749301c1fbb5accd4f3416
Lucy Davinhart || Strawb System <lucy@lucymhdavies.com>
2023-08-02 14:20:39 +0100

    Remove default values for input vars, so we can use dropdowns

--- variables.tf
+++ variables.tf
@@ -1,24 +1,20 @@
 variable "packer_bucket_name" {
   type        = string
-  default     = "webserver"
   description = "Which HCP Packer bucket should we pull our AMI from?"
 }
 
 variable "packer_channel" {
   type        = string
-  default     = "production"
   description = "Which HCP Packer channel should we use for our AMI?"
 }
 
 variable "instance_type" {
   type        = string
-  default     = "t3.micro"
   description = "How big of an instance should we create?"
 }
 
 variable "aws-region" {
   type    = string
-  default = "eu-west-2"
 }
 
 variable "name" {
